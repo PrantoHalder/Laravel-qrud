9dbd4257fd2bfe023dbe1fc96f07038a6b49b365
PrantoHalder <prantohalder117@gmail.com>
2023-05-03 11:53:59 +0600

    Client part update : 1. guest user can register as patient form done 2. log in form for user is completed [03.05.2023]

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[WelcomeController::class,"welcome"])->name('welcome');
+Route::get('/login',[LoginController::class,"login"])->name('login');
+Route::get('/register',[RegisterController::class,"register"])->name('register');
